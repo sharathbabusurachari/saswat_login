fb67c57890636660e82b6d299aff4ed552865bfe
Sharath Babu <sharath.ise@gmail.com>
2024-02-20 12:51:51 +0530

    changes in models and views

--- saswat_cust_info/settings.py
+++ saswat_cust_info/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p$5m!@8&k@4rtj3f+(sxk5xy9!6zpqf$uz)k_c=m1fi%pql-wb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['ci1.saswatfinance.com','20.235.255.141']
 
