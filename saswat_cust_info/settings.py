ddacd5d5d430fa69da920b3cf91b7a68a486b3bd
Sharath Babu <sharath.ise@gmail.com>
2024-02-20 18:04:58 +0530

    updating STATIC_ROOT in settings.py file

--- saswat_cust_info/settings.py
+++ saswat_cust_info/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = '/var/lib/jenkins/workspace/04_Saswat_login_CICD/static'
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'),]
 
 
