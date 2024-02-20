10e7df26c3838ffb51c2ce4f0a824da468d47d39
Sharath Babu <sharath.ise@gmail.com>
2024-02-20 15:59:11 +0530

    updating STATIC_ROOT in settings.py file

--- saswat_cust_info/settings.py
+++ saswat_cust_info/settings.py
@@ -120,11 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-#STATIC_ROOT = '/var/lib/jenkins/workspace/04_Saswat_login_CICD/static'
-#STATICFILES = '/var/lib/jenkins/workspace/04_Saswat_login_CICD/static'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = os.path.join(BASE_DIR,‘static’)
-STATIC_ROOT = os.path.join(BASE_DIR, “static_collect”)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = os.path.join(BASE_DIR, ‘staticfiles’)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
