b7b802682f463497365d179d36e54738f926858d
Sharath Babu <sharath.ise@gmail.com>
2024-02-22 12:22:43 +0530

    deploying gps functionality

--- scripts/envsetup.sh
+++ scripts/envsetup.sh
@@ -6,13 +6,17 @@ then
     echo "Python virtual environment exists." 
 else
     python3 -m venv saswatfinenv
+
 fi
 
 echo $PWD
 source saswatfinenv/bin/activate
+python3 manage.py makemigrations
+python3 manage.py migrate
 
 
 pip3 install -r $WORKSPACE/requirements.txt
+
 
 if [ -d "logs" ] 
 then
