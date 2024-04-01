7c79a6dff2295063032dfd8ca42a6f2e7ec82048
Sharath Babu <sharath.ise@gmail.com>
2024-04-01 11:29:07 +0530

    changes in dataentry_db.models -- bug fixes applied

--- saswat_cust_info/settings.py
+++ saswat_cust_info/settings.py
@@ -146,6 +146,7 @@ DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760  # Set maximum request size to 10 megabyt
 FILE_UPLOAD_MAX_MEMORY_SIZE = 10485760  # Set maximum file upload size to 10 megabytes
 
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
