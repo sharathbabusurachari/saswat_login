82b4dfe4a40d2ea1221b3231375951a928469f67
Sharath Babu <sharath.ise@gmail.com>
2024-04-30 18:06:25 +0530

    fixing new

--- saswat_cust_app/views.py
+++ saswat_cust_app/views.py
@@ -978,8 +978,7 @@ class VleMobileVerificationView(APIView):
                         response = requests.post(url, json=data)
                         print(response)
                         if response.status_code == 200:
-                            VleOtp.objects.create(mobile_no=str(vle_mobile_number), otp_code=otp_code, vle_id_id=vle_id,
-                                              user_id=user_id)
+                            VleOtp.objects.create(mobile_no=str(vle_mobile_number), otp_code=otp_code, vle_id_id=vle_id, user_id=user_id)
                             response_data = {
                                 'vle_id': vle_id,
                                 'status': '00',
