af10c7995b70004ba5707d2de5cbe232d1e644a3
Sharath Babu <sharath.ise@gmail.com>
2024-08-26 11:00:56 +0530

    In Query Added field size increase dynamically 1

--- saswat_cust_app/forms.py
+++ saswat_cust_app/forms.py
@@ -5,14 +5,18 @@ from .models import QueryModel, ShortenedQueries
 
 
 class QueryModelForm(forms.ModelForm):
-    description = forms.CharField(max_length=255, required=False, label="AI Description",
-                                  widget=forms.TextInput(attrs={'readonly': 'readonly'}))
-    additional_info = forms.CharField(max_length=255, required=False, label="AI Additional Information",
-                                      widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+    # description = forms.CharField(max_length=255, required=False, label="AI Description",
+    #                               widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+    # additional_info = forms.CharField(max_length=255, required=False, label="AI Additional Information",
+    #                                   widget=forms.TextInput(attrs={'readonly': 'readonly'}))
 
     class Meta:
         model = QueryModel
         fields = '__all__'
+        widgets = {
+            'description': forms.Textarea(attrs={'class': 'auto-expand', 'readonly': 'readonly'}),
+            'additional_info': forms.Textarea(attrs={'class': 'auto-expand', 'readonly': 'readonly'}),
+        }
 
     class Media:
-        js = ('saswat_cust_app/js/admin_custom.js', 'saswat_cust_app/js/document_query.js')
+        js = ('saswat_cust_app/js/admin_custom.js')
